6b5742feaee8f4e609ff40c2c22314592416cc71
ali alahyarlou <alahyarlou@gmail.com>
2023-02-15 13:25:50 +0330

    responsive student dashboard page and fixed some problem in layout

--- src/pages/students/index.tsx
+++ src/pages/students/index.tsx
@@ -5,47 +5,47 @@ import { Link } from "react-router-dom";
 
 const Students: React.FC = () => {
   return (
-    <div className="my-20 flex items-start justify-center flex-col gap-5 ">
-      <span className="text-[#101114] text-2xl font-semibold">دانشجویان</span>
-      <div className="grid grid-cols-3 gap-5">
+    <div className="mt-16 mx-10 xl:mx-5">
+      <h2 className="text-[#101114] text-2xl font-semibold mb-10 text-center md:text-right">دانشجویان</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         <Link
           to={"/students/initialregestration"}
-          className="px-10 py-6 shadow-[0_1px_2px_0px_rgba(24,24,28,0.04)] flex flex-col items-start justify-center gap-5 border-2 border-[#EEEEF2] rounded-md"
+          className="flex flex-col items-center md:items-start justify-center gap-3 border-2 hover:border-[#4C526D] border-[#EEEEF2] rounded-md px-6 py-5 shadow-[0px_1px_2px_0px_rgba(24,24,28,0.04)] hover:shadow-[0px_1px_6px_0px_rgba(24,24,28,0.04)] duration-200 transition-all"
         >
-          <span className="text-[#101114] font-bold">ثبت نام اولیه ها</span>
-          <div className="text-xs font-semibold">
-            <p className="flex items-center justify-center gap-2">
+          <span className="text-[#101114] font-semibold text-xl mb-2">ثبت نام اولیه ها</span>
+          <div className="font-semibold">
+            <p className="flex items-center justify-center gap-2 mb-2">
               <span className="text-[#E73F3F]">21</span>
               <span className="text-[#4C526D]">دانشجو تایید نشده</span>
             </p>
             <p className="flex items-center justify-center gap-2">
-              <span className="text-[#E73F3F]">35</span>
+              <span className="text-[#52A86E]">35</span>
               <span className="text-[#4C526D]">دانشجو تایید شده</span>
             </p>
           </div>
         </Link>
         <Link
           to={"/students/preregestration"}
-          className="px-10 py-6 shadow-[0_1px_2px_0px_rgba(24,24,28,0.04)] flex flex-col items-start justify-center gap-5 border-2 border-[#EEEEF2] rounded-md"
+          className="flex flex-col items-center md:items-start justify-center gap-3 border-2 hover:border-[#4C526D] border-[#EEEEF2] rounded-md px-6 py-5 shadow-[0px_1px_2px_0px_rgba(24,24,28,0.04)] hover:shadow-[0px_1px_6px_0px_rgba(24,24,28,0.04)] duration-200 transition-all"
         >
-          <span className="text-[#101114] font-bold">پیش ثبت نام ها</span>
-          <div className="text-xs font-semibold">
-            <p className="flex items-center justify-center gap-2">
+          <span className="text-[#101114] font-semibold text-xl mb-2">پیش ثبت نام ها</span>
+          <div className="font-semibold">
+            <p className="flex items-center justify-center gap-2 mb-2">
               <span className="text-[#E73F3F]">21</span>
               <span className="text-[#4C526D]">دانشجو تایید نشده</span>
             </p>
             <p className="flex items-center justify-center gap-2">
-              <span className="text-[#E73F3F]">35</span>
+              <span className="text-[#52A86E]">35</span>
               <span className="text-[#4C526D]">دانشجو تایید شده</span>
             </p>
           </div>
         </Link>
         <Link
           to={"/students/form"}
-          className="px-10 py-6 shadow-[0_1px_2px_0px_rgba(24,24,28,0.04)] flex flex-col items-start justify-center gap-5 border-2 border-[#EEEEF2] rounded-md"
+          className="flex flex-col items-center md:items-start justify-center gap-3 border-2 hover:border-[#4C526D] border-[#EEEEF2] rounded-md px-6 py-5 shadow-[0px_1px_2px_0px_rgba(24,24,28,0.04)] hover:shadow-[0px_1px_6px_0px_rgba(24,24,28,0.04)] duration-200 transition-all"
         >
-          <span className="text-[#101114] font-bold">فرم ها</span>
-          <div className="text-xs font-semibold flex flex-col items-start justify-center">
+          <span className="text-[#101114] font-semibold text-xl mb-2">فرم ها</span>
+          <div className="font-semibold flex flex-col items-start justify-center gap-y-3 text-sm">
             <span className="text-[#4C526D]">فرم شماره دو ، سه ، چهار</span>
             <span className="text-[#4C526D]">تایید گزارش هفتگی</span>
             <span className="text-[#4C526D]">نامه تمام کارآموزی</span>
