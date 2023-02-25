8f7f35d1858fc6f0af5d11a97b567357ea735768
mohsen noury <66866297+MNGH-27@users.noreply.github.com>
2023-02-26 01:20:05 +0330

    fix pagination style

--- src/components/common/tableWrapper/pagination/index.tsx
+++ src/components/common/tableWrapper/pagination/index.tsx
@@ -8,31 +8,29 @@ interface TablePaginationProps {
 }
 
 const TablePagination: React.FC<TablePaginationProps> = ({ meta }) => {
-  console.log("meta : ", meta);
-
   return (
     <div className="flex items-center justify-between w-full">
       <p className="text-sm text-[#5F5F61]">
-        نشان دادن{" "}
+        نشان دادن
         <span className="text-[#222124] font-medium">
           {meta.current_page * meta.per_page - meta.per_page + 1}
-        </span>{" "}
+        </span>
         از
         <span className="text-[#222124] font-medium">
           {meta.current_page * meta.per_page < meta.total_records
             ? meta.current_page * meta.per_page
             : meta.total_records}
-        </span>{" "}
+        </span>
         از
-        <span className="text-[#222124] font-medium">
-          {meta.total_records}
-        </span>{" "}
+        <span className="text-[#222124] font-medium">{meta.total_records}</span>
         نتیجه
       </p>
       <div className="flex items-center justify-center gap-5">
         <button
           className={`${
-            meta.current_page === 1 ? "opacity-50" : "hover:bg-[#E6E6E6]"
+            meta.current_page === 1
+              ? "opacity-50 cursor-default"
+              : "hover:bg-[#E6E6E6]"
           } px-3 py-2 border-2 border-[#E6E6E6] shadow-[0_1px_2px_0px_rgba(24,24,28,0.04)] rounded-xl text-sm duration-200 text-[#222124]`}
         >
           قبلی
@@ -40,7 +38,7 @@ const TablePagination: React.FC<TablePaginationProps> = ({ meta }) => {
         <button
           className={`${
             meta.current_page === meta.total_pages
-              ? "opacity-50"
+              ? "opacity-50 cursor-default"
               : "hover:bg-[#E6E6E6]"
           } px-3 py-2 border-2 border-[#E6E6E6] shadow-[0_1px_2px_0px_rgba(24,24,28,0.04)] rounded-xl text-sm duration-200 text-[#222124]`}
         >
