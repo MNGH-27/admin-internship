1b4abcc41fdf556e28524f732d753f5013e8abe1
mohsen noury <66866297+MNGH-27@users.noreply.github.com>
2023-02-28 00:52:55 +0330

    fix rejectStudent Modal ( connect to backend)

--- src/components/pages/students/initialregestration/rejectStudentModal/index.tsx
+++ src/components/pages/students/initialregestration/rejectStudentModal/index.tsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
 //component
 import ModalWrapper from "../../../../common/modalWrapper";
-
+import LoadingButton from "../../../../common/loadingBtn";
 //svg
-import { ReactComponent as CloseSvg } from "./../../../../assets/icons/svg/close-circle.svg";
+import { ReactComponent as CloseSvg } from "./../../../../../assets/icons/svg/close-circle.svg";
 
-const RejectStudentModal: React.FC = () => {
+//interface
+interface rejectStudentModalProps {
+  closeModalHandler: (reqCondition?: boolean, detail?: string) => void;
+  isLoading: boolean;
+}
+
+const RejectStudentModal: React.FC<rejectStudentModalProps> = ({
+  closeModalHandler,
+  isLoading,
+}) => {
+  const textboxContainer = useRef<HTMLTextAreaElement>(null);
+
   return (
-    <ModalWrapper
-      isShowedModal={true}
-      onCloseModal={() => {
-        console.log("hello world");
-      }}
-    >
+    <ModalWrapper isShowedModal={true} onCloseModal={() => closeModalHandler()}>
       <div className="flex flex-col items-start justify-center gap-3">
-        <button>
+        <button onClick={() => closeModalHandler()}>
           <CloseSvg />
         </button>
         <span className="text-[#222124] text-xl font-semibold">
@@ -27,16 +33,29 @@ const RejectStudentModal: React.FC = () => {
         <div className="flex flex-col items-start justify-center gap-2 w-full my-3">
           <label className="text-[#5F5F61] text-xs">علت رد دانشجو</label>
           <textarea
+            ref={textboxContainer}
             className="shadow-[0_1px_2px_0px_rgba(24,24,28,0.04)] w-full border-2 border-[#E6E6E6] rounded-xl min-h-[100px] max-h-[200px] text-sm placeholder:text-xs p-2 outline-none"
             placeholder="توضیحات . . ."
             name="reject"
           />
         </div>
         <div className="flex justify-end gap-2 w-full">
-          <button className="px-4 py-2 rounded-md text-white bg-[#E73F3F] border-2 border-[#E73F3F] hover:text-[#E73F3F] hover:bg-white duration-200">
+          <LoadingButton
+            onClickHandler={() =>
+              closeModalHandler(true, textboxContainer.current?.value)
+            }
+            // buttonClass="text-white"
+            paddingClass="px-4 py-2"
+            mainBgColor="#E73F3F"
+            hoverBgColor="#fff"
+            isLoading={isLoading}
+          >
             رد کردن
-          </button>
-          <button className="shadow-[0_1px_2px_0px_rgba(24,24,28,0.04)] px-4 py-2 rounded-md border-2 border-[#E6E6E6] hover:bg-[#E6E6E6] duration-200">
+          </LoadingButton>
+          <button
+            onClick={() => closeModalHandler()}
+            className="shadow-[0_1px_2px_0px_rgba(24,24,28,0.04)] px-4 py-2 rounded-md border-2 border-[#E6E6E6] hover:bg-[#E6E6E6] duration-200"
+          >
             بستن
           </button>
         </div>
