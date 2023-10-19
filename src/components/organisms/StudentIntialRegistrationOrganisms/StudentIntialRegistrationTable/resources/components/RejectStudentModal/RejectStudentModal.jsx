f8e36f97a98f226b65edb6363f4b3b79d184a376
mohsen noury <66866297+MNGH-27@users.noreply.github.com>
2023-10-19 19:41:34 +0330

    fix reject student modal

--- src/components/organisms/StudentIntialRegistrationOrganisms/StudentIntialRegistrationTable/resources/components/RejectStudentModal/RejectStudentModal.jsx
+++ src/components/organisms/StudentIntialRegistrationOrganisms/StudentIntialRegistrationTable/resources/components/RejectStudentModal/RejectStudentModal.jsx
@@ -1,29 +1,71 @@
+'use client'
+
 import { Button, Input, Modal } from '@atom/index'
+import { putUnVarifyStudentInitialRegestration } from '@core/services'
+import { Formik } from 'formik'
 import { IoPersonRemoveSharp } from 'react-icons/io5'
+import * as yup from 'yup'
+
+import { useMutation } from 'react-query'
+import { FromContainer } from '@molecule/index'
 
 const RejectStudentModal = ({ isShow, onClose, data }) => {
+  const schema = yup.object().shape({
+    description: yup.string().required('مقدار توضیحات الزامی است'),
+  })
+
+  const { mutate, isLoading } = useMutation({
+    mutationKey: ['unverify_initial_registration'],
+    mutationFn: (detail) =>
+      putUnVarifyStudentInitialRegestration({
+        student_id: data?.student_number,
+        detail,
+      }),
+  })
+
   return (
     <Modal isShow={isShow} onClose={onClose}>
-      <div className="flex flex-col items-start justify-center gap-3">
-        <span className="text-[#222124] text-xl font-semibold">
-          رد کردن دانشجو
-        </span>
-        <span className="text-[#5F5F61] text-xs">
-          آیا از رد کردن دانشجو به شماره دانشجویی {data?.studentNumber} مطمئن
-          هستید؟
-        </span>
-        <div className="flex flex-col items-start justify-center gap-2 w-full my-3">
-          <label className="text-[#5F5F61] text-xs">علت رد دانشجو</label>
-          <Input type="textArea" placeholder="توضیحات . . ." name="reject" />
-        </div>
-        <Button
-          icon={<IoPersonRemoveSharp size={20} />}
-          type="primary"
-          className="!bg-red-700"
-        >
-          رد کردن
-        </Button>
-      </div>
+      <Formik
+        initialValues={{ ...schema.getDefault() }}
+        validationSchema={schema}
+        onSubmit={(values) => {
+          mutate(values)
+        }}
+      >
+        {({ values, handleSubmit, handleChange }) => (
+          <form
+            method="post"
+            className="flex flex-col items-start justify-center gap-3"
+          >
+            <span className="text-[#222124] text-xl font-semibold">
+              رد کردن دانشجو
+            </span>
+            <span className="text-[#5F5F61] text-xs">
+              آیا از رد کردن دانشجو &quot;{data?.name} {data?.last_name}&quot;
+              به شماره دانشجویی {data?.student_number} مطمئن هستید؟
+            </span>
+            <FromContainer label="توضیحات" name="description">
+              <Input
+                name="description"
+                onChange={handleChange}
+                value={values.description}
+                type="textArea"
+                placeholder="توضیحات . . ."
+              />
+            </FromContainer>
+            <Button
+              loading={isLoading}
+              htmlType="submit"
+              onClick={handleSubmit}
+              icon={<IoPersonRemoveSharp size={20} />}
+              type="primary"
+              className="!bg-red-700"
+            >
+              رد کردن
+            </Button>
+          </form>
+        )}
+      </Formik>
     </Modal>
   )
 }
--- src/core/services/index.js
+++ src/core/services/index.js
@@ -1,4 +1,10 @@
 import { loginUserHttp } from './apis/login_user.apis'
 import { getUserInfoHttp } from './apis/user/get_user_info.api'
 import { getInitialRegestrationStundets } from './apis/student/get_initial_registration_list.api'
-export { getInitialRegestrationStundets, loginUserHttp, getUserInfoHttp }
+import { putUnVarifyStudentInitialRegestration } from './apis/student/put_unverify_student.api'
+export {
+  getInitialRegestrationStundets,
+  putUnVarifyStudentInitialRegestration,
+  loginUserHttp,
+  getUserInfoHttp,
+}
